37c101384f0ecefe78968a0f7ab6b19dbe851584
Aaron Peter Viju <aaronpeterviju11@gmail.com>
2026-01-21 09:44:06 +0530

    commit

--- UserManagementAPI/obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
+++ UserManagementAPI/obj/Debug/net10.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d2f157fbb7f274a65a867aee041cf139c33b3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
